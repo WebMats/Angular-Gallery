7dbbb6039ff6c25fea72e0b56fced8dee6a3696b
WebMats <lorlando127@gmail.com>
2019-05-04 22:32:38 -0400

    added mime type validator to photo upload to firebase storage

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -4,6 +4,7 @@ const normalizePost = (post) => ({
     id: post.id,
     title: post.title,
     content: post.content,
+    imageURL: post.imageURL
 })
 
 module.exports = {
@@ -15,11 +16,23 @@ module.exports = {
             console.log(err);
         }
     },
+    post: async ({ id }) => {
+        try {
+            const post = await Post.findById(id);
+            return normalizePost(post);
+        } catch (err) {
+            console.log(err)
+        }
+    },
     createPost: async ({ postInput }, req) => {
         // if (false) {
         //     console.log('if TRUE')
         // }
-        const post = new Post({ title: postInput.title, content: postInput.content });
+        const post = new Post({ 
+            title: postInput.title, 
+            content: postInput.content, 
+            imageURL: postInput.imageURL 
+        });
         try {
             const savedPost = await post.save();
             return savedPost.id
@@ -27,9 +40,18 @@ module.exports = {
             console.log(err)
         }
     },
+    updatePost: async ({ postId, updateInput }) => {
+        const updatedPost = new Post({ _id: postId, ...updateInput });
+        try {
+            await Post.updateOne({ _id: postId }, updatedPost)
+            return normalizePost(updatedPost);
+        } catch (err) {
+            console.log(err)
+        }
+    },
     deletePost: async ({ postId }) => {
         try {
-            await Post.deleteOne({ id: postId });
+            await Post.deleteOne({ _id: postId });
             return 'Deleted'
         } catch (err) {
             console.log(err);
