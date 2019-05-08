de9ed6482472e119b90d99e2831be818710fdf85
WebMats <lorlando127@gmail.com>
2019-05-08 00:05:31 -0400

    deleted aws files, added client dir to 'Build' step

--- codedeploy/after_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /ang-gal
-npm install
--- codedeploy/before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export app_root=/ang-gal
-if [ -d "$app_root"]; then
-    rm -rf /ang-gal
-    mkdir -p /ang-gal
-else 
-    mkdir -p /ang-gal
-fi
