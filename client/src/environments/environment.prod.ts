ae57ae3078b8f7cce951b7bb38a85b944ae8f386
WebMats <lorlando127@gmail.com>
2019-05-08 00:13:27 -0400

    added firebase config to production env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzz7Uxc9i8lP4Ziq8AVZh2danrMastBvw",
+    authDomain: "angular-f1b31.firebaseapp.com",
+    databaseURL: "https://angular-f1b31.firebaseio.com",
+    projectId: "angular-f1b31",
+    storageBucket: "angular-f1b31.appspot.com",
+    messagingSenderId: "438042561829"
+  },
 };
