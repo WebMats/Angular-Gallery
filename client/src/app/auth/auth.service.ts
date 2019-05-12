67112eb0a9d47b4dcc2476a6d554f114a0bfdd0a
WebMats <lorlando127@gmail.com>
2019-05-12 02:18:04 -0400

    handling authentication errors

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -13,36 +13,76 @@ import { Subject, Observable } from 'rxjs';
 })
 export class AuthService {
   isAuthenticated: Observable<any>;
-  private authStateChanged = new Subject<User | null >();
+  isSignUp: boolean = false;
   private token: string = '';
   private user: User | null;
+  private authStateChanged = new Subject<User | null >();
+  hasError: { message: string | null };
+  private hasErrorChanged = new Subject<{message}>();
+
   constructor(private router: Router, private afAuth: AngularFireAuth, private http: HttpClient) { 
     this.isAuthenticated = this.afAuth.authState;
     this.afAuth.auth.onAuthStateChanged(user => {
-      if (user) {
-        this.signInUser(<any>user);
-      }
+      if (!user) return;
+      if (this.isSignUp) return;
+      this.signInUser(<any>user);
     })
   }
 
   getToken = (): string => this.token;
   getAuthStateListener = () => this.authStateChanged.asObservable();
+  getHasErrorListener = () => this.hasErrorChanged.asObservable();
   getUser = () => this.user;
+  getHasError = () => this.hasError;
   googleSignIn = async () => {
+    this.isSignUp = false;
     const provider = new auth.GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" })
     const credentials = await this.afAuth.auth.signInWithPopup(provider);
-    return credentials.additionalUserInfo.isNewUser ? this.signUpUser(<any>credentials.user) : this.signInUser(<any>credentials.user);
+    if (credentials.additionalUserInfo.isNewUser) {
+      credentials.user.delete();
+      this.router.navigate(['/signup']);
+      this.setAndPropagateErrorState({ message: 'This account does not exist, create it now.' });
+    } else {
+      this.signInUser(<any>credentials.user);
+    }
+  }
+  googleSignUp = async () => {
+    this.isSignUp = true;
+    const provider = new auth.GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" })
+    const credentials = await this.afAuth.auth.signInWithPopup(provider);
+    if (!credentials.additionalUserInfo.isNewUser) {
+      this.afAuth.auth.signOut().then(_ => {
+        this.token = '';
+        this.router.navigate(['/login']);
+        this.setAndPropagateErrorState({ message: 'This account exists, we redirected you to login.' });
+      }).catch(err => {
+        console.log(err)
+      });
+    } else {
+      this.signUpUser(<any>credentials.user);
+    }
   }
 
   emailAndPasswordSignIn = async (email: string, password: string) => {
-    const credentials = await this.afAuth.auth.signInWithEmailAndPassword(email, password);
-    return this.signInUser(<any>credentials.user);
+    this.isSignUp = false;
+    try {
+      const credentials = await this.afAuth.auth.signInWithEmailAndPassword(email, password);
+      this.signInUser(<any>credentials.user);
+    } catch (err) {
+      this.setAndPropagateErrorState({ message: err.message });
+    }
   }
 
   emailAndPasswordSignUp = async (email: string, password: string) => {
-    const credentials = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
-    return this.signUpUser(<any>credentials.user);
+    this.isSignUp = true;
+    try {
+      const credentials = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+      this.signUpUser(<any>credentials.user);
+    } catch (err) {
+      this.setAndPropagateErrorState({ message: err.message });
+    }
   }
 
   private signUpUser = ({ email, uid: id, ra: token }: Credentials) => {
@@ -50,7 +90,8 @@ export class AuthService {
     this.http.post<{data}>('http://localhost:5000/graphql', {query: signUpGQL(email, id, token)}).subscribe(({data}) => {
       const { mongoId: id, firebaseId, email } = data.signup;
       if (!!id) {
-        this.propagateAuthStateAndNavigate({id, firebaseId, email})
+        this.propagateAuthStateAndNavigate({id, firebaseId, email});
+        this.setAndPropagateErrorState({ message: null });
       }
     })
   }
@@ -60,6 +101,7 @@ export class AuthService {
       const { mongoId: id, firebaseId, email } = data.signin;
       if (!!id) {
         this.propagateAuthStateAndNavigate({id, firebaseId, email})
+        this.setAndPropagateErrorState({ message: null });
       }
     })
   }
@@ -76,5 +118,9 @@ export class AuthService {
     this.router.navigate(['/'])
     this.user = user;
     this.authStateChanged.next(this.user);
+  }
+  setAndPropagateErrorState = (error) => {
+    this.hasError = error;
+    this.hasErrorChanged.next(this.hasError);
   }
 }
--- client/src/app/auth/signup/signup.component.ts
+++ client/src/app/auth/signup/signup.component.ts
@@ -1,16 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";``
 import { NgForm } from "@angular/forms";
 import { AuthService } from "../auth.service";
+import { Subscription } from "rxjs";
 
 
 @Component({
     templateUrl: './signup.component.html',
     styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit, OnDestroy {
     isLoading: boolean = false;
+    fbAuthError: string = '';
+    subToError: Subscription;
 
     constructor(private fbAuth: AuthService) {}
+
+    ngOnInit() {
+        this.fbAuthError = !!this.fbAuth.getHasError() && this.fbAuth.getHasError().message;
+        this.subToError = this.fbAuth.getHasErrorListener().subscribe(err => {
+            this.fbAuthError = err.message;
+        })
+    }
 
     onSignUpWithEmailAndPassword = (form: NgForm) => {
         this.isLoading = true;
@@ -18,7 +28,10 @@ export class SignupComponent {
     }
     onSignUpWithGoogle = () => {
         this.isLoading = true;
-        this.fbAuth.googleSignIn();
+        this.fbAuth.googleSignUp();
     }
 
+    ngOnDestroy() {
+        this.subToError.unsubscribe();
+    }
 }
